7af24bed25085637bc35be5b9e972d65b0ff13c7
bluelim <chinapoopoo@gmail.com>
2017-07-04 18:07:40 +0900

    폰트적용

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
   titles: Array<string> = ["기본정보", "유수분관리", "민감관리", "손상관리", "피부결관리"];
-  nowIdx: number = 0;
+  nowIdx: number = 1;
   objIdx: number = 14;
   total: number = 45;
 
