e2fbfb94d5ea5c48b26dbb02488c48229835fcf7
blue_lim <chinapoopoo@gmail.com>
2017-07-06 00:05:30 +0900

    시도는 해봤지만...

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 declare var $: any;
 
@@ -7,7 +7,7 @@ declare var $: any;
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
   titles: Array<string> = ["기본정보", "유수분관리", "민감관리", "손상관리", "피부결관리"];
   nowIdx: number = 1;
   objIdx: number = 14;
@@ -16,6 +16,8 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit(): void {
     $(window).on("load", () => {
       $("a[rel='m_PageScroll2id']").mPageScroll2id({
         layout: "horizontal",
