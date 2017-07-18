e6004dae17feb10c3881fd2867a340535f26f463
bluelim <chinapoopoo@gmail.com>
2017-07-18 18:27:09 +0900

    냐냐

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { ResultComponent } from './pages/result/result.component';
 import { FormComponent } from './pages/form/form.component';
 
 export const routes:Routes = [
-  { path: '', component: FormComponent},
+  { path: '', component: ResultComponent},
 ];
 
 export const Routing = RouterModule.forRoot(routes);
--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result.component.scss']
 })
 export class ResultComponent implements OnInit {
+  barLabel: Array<any> = ["유수분관리", "민감관리", "손상관리", "노화관리", "탄력관리"];
+  barAmt: Array<any> = [78, 74, 63, 79, 57];
+  type = 'radar';
+  data = {
+    labels: this.barLabel,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: 'rgba(152, 201, 236, 1)',
+        borderColor: 'rgba(152, 201, 236, 1)',
+        pointBackgroundColor: 'rgba(152, 201, 236, 0)',
+        pointBorderColor: 'rgba(152, 201, 236, 0)',
+        data: this.barAmt,
+        fill: true
+      }
+    ]
+  };
+  options = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
