9da2a27a9688492e1b2a5543f823184a387aa322
blue_lim <chinapoopoo@gmail.com>
2017-07-18 23:41:40 +0900

    어려운건 다 지나갔다

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -8,24 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class ResultComponent implements OnInit {
   barLabel: Array<any> = ["유수분관리", "민감관리", "손상관리", "노화관리", "탄력관리"];
   barAmt: Array<any> = [78, 74, 63, 79, 57];
+  cards: Array<any> = [
+    {image: "assets/images/icon_for_berry.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"},
+    {image: "assets/images/icon_for_chemical.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"},
+    {image: "assets/images/icon_for_chemical2.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"},
+    {image: "assets/images/icon_for_plants.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"}
+  ];
   type = 'radar';
   data = {
     labels: this.barLabel,
     datasets: [
       {
-        label: "My First dataset",
+        label: "",
         backgroundColor: 'rgba(152, 201, 236, 1)',
         borderColor: 'rgba(152, 201, 236, 1)',
         pointBackgroundColor: 'rgba(152, 201, 236, 0)',
         pointBorderColor: 'rgba(152, 201, 236, 0)',
         data: this.barAmt,
-        fill: true
+        fill: true,
       }
     ]
   };
   options = {
     responsive: true,
     maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    tooltips: {
+      enabled: false
+    },
+    scale: {
+        // Hides the scale
+        display: true,
+        ticks: {
+          display: false,
+          min: 0,
+          max: 100,
+        },
+    }
   };
 
   constructor() { }
