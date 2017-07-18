72a4d806a98496c9f8be9c9419f1ca18313c45e9
blue_lim <chinapoopoo@gmail.com>
2017-07-19 00:21:33 +0900

    dd

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -9,10 +9,10 @@ export class ResultComponent implements OnInit {
   barLabel: Array<any> = ["유수분관리", "민감관리", "손상관리", "노화관리", "탄력관리"];
   barAmt: Array<any> = [78, 74, 63, 79, 57];
   cards: Array<any> = [
-    {image: "assets/images/icon_for_berry.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"},
-    {image: "assets/images/icon_for_chemical.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"},
-    {image: "assets/images/icon_for_chemical2.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"},
-    {image: "assets/images/icon_for_plants.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf"}
+    {image: "assets/images/icon_for_plants.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf", "imgText": "가자가자"},
+    {image: "assets/images/icon_for_berry.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf", "imgText": "가자가자"},
+    {image: "assets/images/icon_for_chemical.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf", "imgText": "가자가자"},
+    {image: "assets/images/icon_for_chemical2.png", content: "fdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasffdasfsdafdsafddddddddddddddddddddddddddddddddddsdsdsdsdsasf", "imgText": "가자가자"},
   ];
   type = 'radar';
   data = {
