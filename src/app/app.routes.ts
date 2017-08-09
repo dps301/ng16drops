b3eb4647133e7a9684fc8b36a335ae08f1ea13fb
blue_lim <chinapoopoo@gmail.com>
2017-08-10 00:41:33 +0900

    일단..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { ResultComponent } from './pages/result/result.component';
 import { FormComponent } from './pages/form/form.component';
 
 export const routes:Routes = [
-  { path: '', component: ResultComponent},
+  { path: '', component: FormComponent},
 ];
 
 export const Routing = RouterModule.forRoot(routes);
