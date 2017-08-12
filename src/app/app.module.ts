35a1c65fce13a677351f6b277d0e823c8dd02979
blue_lim <chinapoopoo@gmail.com>
2017-08-12 18:27:55 +0900

    가져가요

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FormInputComponent } from './components/form-input/form-input.component
 
 import { ChartModule } from 'angular2-chartjs';
 import { UserComponent } from './pages-admin/user/user.component';
+import { ItemComponent } from './pages-admin/item/item.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { UserComponent } from './pages-admin/user/user.component';
     FormCheckComponent,
     FormInputComponent,
     UserComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ResultComponent } from './pages/result/result.component';
 import { FormComponent } from './pages/form/form.component';
 import { UserComponent } from './pages-admin/user/user.component'
+import { ItemComponent } from './pages-admin/item/item.component';
 
 export const routes:Routes = [
   { path: '', component: FormComponent},
   { path: 'result/:id', component: ResultComponent},
   { path: 'admin/user', component: UserComponent},
+  { path: 'admin/item', component: ItemComponent},
 ];
 
 export const Routing = RouterModule.forRoot(routes);
