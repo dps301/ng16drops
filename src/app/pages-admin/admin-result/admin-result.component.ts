42319ff2a473d6b3a947e917200949cdc3335204
genesis <genesis041835@gmail.com>
2017-08-13 21:20:56 +0900

    asd

--- src/app/pages-admin/admin-result/admin-result.component.ts
+++ src/app/pages-admin/admin-result/admin-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+
 
 @Component({
   selector: 'app-admin-result',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminResultComponent implements OnInit {
 
-  constructor() { }
+  result: any = [];
+  info: any = [];
+
+  constructor(private http: HttpService) { }
 
   ngOnInit() {
+    this.load();
+  }
+  load() {
+      this.http.get('/admin/result')
+          .subscribe(
+              data => {
+                console.log(data)
+                this.result = data.json();
+              }
+          );
+  }
+  find(resultNo:number) {
+      this.http.get('/admin/result/detail?resultNo='+resultNo)
+          .subscribe(
+              data => {
+                this.info = data.json().item;
+                console.log(this.info)
+              }
+          );
+  }
+  save(resultNo,name){
+      this.http.put('/admin/result',{resultNo:resultNo,name:name})
+          .subscribe(
+              data => {
+                this.load();
+              }
+          );
+  }
+  save2(item){
+      this.http.put('/admin/result/detail',
+          {
+            result_manage_no:item.result_manage_no,
+            title:item.title,
+            con:item.con,
+            icon_url:item.icon_url
+          })
+          .subscribe(
+              data => {
+                this.load();
+              }
+          );
   }
 
 }
