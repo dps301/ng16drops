8167968a2fafe32754c22db7d98324c3777a7c0e
blue_lim <chinapoopoo@gmail.com>
2017-08-16 23:39:32 +0900

    굿

--- src/app/pages-admin/admin-result/admin-result.component.ts
+++ src/app/pages-admin/admin-result/admin-result.component.ts
@@ -20,64 +20,46 @@ export class AdminResultComponent implements OnInit {
   }
   load() {
       this.http.get('/admin/result')
-          .subscribe(
-              data => {
-                console.log(data)
-                this.result = data.json();
-              }
-          );
+      .subscribe(
+        data => {
+          console.log(data)
+          this.result = data.json();
+        }
+      );
   }
   find(resultNo:number) {
       this.http.get('/admin/result/detail?resultNo='+resultNo)
-          .subscribe(
-              data => {
-                this.info = data.json().item;
-                this.detail = data.json();
-                console.log(this.info)
-              }
-          );
+      .subscribe(
+        data => {
+          this.info = data.json().item;
+          this.detail = data.json();
+          console.log(this.info)
+        }
+      );
   }
   save(resultNo,name){
-      this.http.put('/admin/result',{resultNo:resultNo,name:name})
-          .subscribe(
-              data => {
-                this.load();
-              }
-          );
+      this.http.put('/admin/result',{resultNo: resultNo, name: name})
+      .subscribe(
+        data => {
+          this.load();
+        }
+      );
   }
   save2(item){
-      this.http.put('/admin/result/detail',
-          {
-            result_manage_no:item.result_manage_no,
-            title:item.title,
-            con:item.con,
-            icon_url:item.icon_url
-          })
-          .subscribe(
-              data => {
-                this.load();
-              }
-          );
+      this.http.put('/admin/result/detail', {result_manage_no: item.result_manage_no, title: item.title, con: item.con, icon_url:item.icon_url})
+      .subscribe(
+        data => {
+          this.load();
+        }
+      );
   }
   save3(item){
-      this.http.put('/admin/result/info',
-          {
-            resultNo:item.form_result_no,
-            add:item.add,
-            con1:item.con1,
-            con1_descript:item.con1_descript,
-            con2:item.con2,
-            con2_descript:item.con2_descript,
-            con3:item.con3,
-            con3_descript:item.con3_descript,
-            con4:item.con4,
-            con4_descript:item.con4_descript
-          })
-          .subscribe(
-              data => {
-                this.load();
-              }
-          );
+      this.http.put('/admin/result/info', {resultNo: item.form_result_no, add: item.add, con1: item.con1, con1_descript: item.con1_descript, con2: item.con2, con2_descript: item.con2_descript, con3: item.con3, con3_descript: item.con3_descript, con4: item.con4, con4_descript: item.con4_descript, link: item.link, caution: item.caution})
+    .subscribe(
+        data => {
+          this.load();
+        }
+    );
   }
 
 }
